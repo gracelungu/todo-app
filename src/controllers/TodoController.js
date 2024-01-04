eadcc66d72a560055d3ae877af25f75585fac864
Grace Lungu <grace.lungu.bw@gmail.com>
2024-01-04 21:04:49 +0100

    Add new TodoController

---
+++ src/controllers/TodoController.js
@@ -0,0 +1,11 @@
+/* This file contains the TodoController for the todo-app repository. */
+
+class TodoController {
+  constructor() {
+    // Initialize controller properties here
+  }
+
+  // Methods for todo management come following here
+}
+
+export default new TodoController();
